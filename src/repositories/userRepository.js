5d53dab91ccc289b2647f997681ee78fc8d4193d
Arpan25AK <akarpan2005@gmail.com>
2026-05-25 20:13:26 +0530

    finished the repo codebase

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,23 +1,34 @@
-// Repository — like @Repository in Spring
-const { users, getNextId } = require('../models/user')
+const User = require('../models/user')
 
-const findAll = () => users
-const findById = (id) => users.find(u => u.id === Number(id))
-const save = (data) => {
-  const user = { id: getNextId(), ...data }
+
+const users = []
+
+const findAll = () =>{
+  return users
+}
+
+const findById = (id) => { 
+  return users.find( u => u.id === id)
+}
+
+const findByName = (name) => {
+  return users.find(u => u.name === name)
+}
+
+const findByEmail = (email) =>{
+  return users.find(u => u.email === email)
+}
+
+const findByPost = (position) => {
+  return users.filter(u => u.position === position)
+}
+
+const findByPostAndName = (position, name) =>{
+  return users.filter( u => u.position === position && u.name === name)
+}
+
+const saveUser = (id, name, email, position) => {
+  const user = new User(id, name, email, position)
   users.push(user)
   return user
 }
-const update = (id, data) => {
-  const index = users.findIndex(u => u.id === Number(id))
-  if (index === -1) return null
-  users[index] = { ...users[index], ...data }
-  return users[index]
-}
-const remove = (id) => {
-  const index = users.findIndex(u => u.id === Number(id))
-  if (index === -1) return false
-  users.splice(index, 1)
-  return true
-}
-module.exports = { findAll, findById, save, update, remove }
