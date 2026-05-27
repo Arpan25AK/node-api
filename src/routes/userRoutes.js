f4a723b90b17bbfbd5360c01eeb5e99fd6960be0
Arpan25AK <akarpan2005@gmail.com>
2026-05-27 15:24:51 +0530

    made routes placement changes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,50 +2,50 @@ const express = require('express')
 const router = express.Router()
 const userService = require('../services/userService')
 
-router.get("/users", (req , res ) => {
-  const result  = userService.getAllUsers()
+router.get("/users", (req, res) => {
+  const result = userService.getAllUsers()
   res.json(result)
 })
 
-router.get("/users/:id", (req , res ) => {
-  const result  = userService.findById(req.params.id)
+router.get("/users/email", (req, res) => {
+  const result = userService.findByEmail(req.query.email)
   res.json(result)
 })
 
-router.get("/users/email" ,(req , res ) => {
-  const result  = userService.findByEmail(req.query.email)
+router.get("/users/position", (req, res) => {
+  const result = userService.findByPost(req.query.position)
   res.json(result)
 })
 
-router.get("/users/position", (req , res ) => {
-  const result  = userService.findByPost(req.query.position)
+router.get("/users/searchName", (req, res) => {
+  const result = userService.findByName(req.query.name)
   res.json(result)
 })
 
-router.get("/users/searchName", (req , res ) => {
-  const result  = userService.findByName(req.query.name)
+router.get("/users/postAndName", (req, res) => {
+  const result = userService.findByPostAndName(req.query.position, req.query.name)
   res.json(result)
 })
 
-router.get("/users/postAndName", (req , res ) => {
-  const result  = userService.findByPostAndName(req.query.position, req.query.name)
+router.get("/users/:id", (req, res) => {
+  const result = userService.findById(req.params.id)
   res.json(result)
 })
 
-router.delete("/users/:id", (req , res ) => {
-  const result  = userService.removeUser(req.params.id)
+router.delete("/users/:id", (req, res) => {
+  const result = userService.removeUser(req.params.id)
   res.json(result)
 })
 
-router.put("/users/:id", (req , res ) => {
-  const {id, name, email, position} = req.body
-  const result  = userService.updateUserById(req.params.id, name, email, position)
+router.put("/users/:id", (req, res) => {
+  const { name, email, position } = req.body
+  const result = userService.updateUserById(req.params.id, name, email, position)
   res.json(result)
 })
 
-router.post("/users", (req , res ) => {
-  const {id , name, email, position} = req.body
-  const result  = userService.saveUser(id, name, email, position)
+router.post("/users", (req, res) => {
+  const { id, name, email, position } = req.body
+  const result = userService.saveUser(id, name, email, position)
   res.json(result)
 })
 
