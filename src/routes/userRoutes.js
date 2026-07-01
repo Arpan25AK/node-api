0a92444e24dff211b0c31852a5a9903c48fd6cfc
Arpan25AK <akarpan2005@gmail.com>
2026-07-01 17:59:58 +0530

    made the route service implement async and await to handle prisma

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,50 +2,50 @@ const express = require('express')
 const router = express.Router()
 const userService = require('../services/userService')
 
-router.get("/users", (req, res) => {
-  const result = userService.getAllUsers()
+router.get("/users", async(req, res) => {
+  const result = await userService.getAllUsers()
   res.json(result)
 })
 
-router.get("/users/email", (req, res) => {
-  const result = userService.findByEmail(req.query.email)
+router.get("/users/email", async(req, res) => {
+  const result = await userService.findByEmail(req.query.email)
   res.json(result)
 })
 
-router.get("/users/position", (req, res) => {
-  const result = userService.findByPost(req.query.position)
+router.get("/users/position", async(req, res) => {
+  const result = await userService.findByPost(req.query.position)
   res.json(result)
 })
 
-router.get("/users/searchName", (req, res) => {
-  const result = userService.findByName(req.query.name)
+router.get("/users/searchName", async (req, res) => {
+  const result = await userService.findByName(req.query.name)
   res.json(result)
 })
 
-router.get("/users/postAndName", (req, res) => {
-  const result = userService.findByPostAndName(req.query.position, req.query.name)
+router.get("/users/postAndName", async(req, res) => {
+  const result = await userService.findByPostAndName(req.query.position, req.query.name)
   res.json(result)
 })
 
-router.get("/users/:id", (req, res) => {
-  const result = userService.findById(req.params.id)
+router.get("/users/:id", async (req, res) => {
+  const result = await userService.findById(req.params.id)
   res.json(result)
 })
 
-router.delete("/users/:id", (req, res) => {
-  const result = userService.removeUser(req.params.id)
+router.delete("/users/:id", async (req, res) => {
+  const result = await userService.removeUser(req.params.id)
   res.json(result)
 })
 
-router.put("/users/:id", (req, res) => {
+router.put("/users/:id", async (req, res) => {
   const { name, email, position } = req.body
-  const result = userService.updateUserById(req.params.id, name, email, position)
+  const result = await userService.updateUserById(req.params.id, name, email, position)
   res.json(result)
 })
 
-router.post("/users", (req, res) => {
+router.post("/users", async (req, res) => {
   const { id, name, email, position } = req.body
-  const result = userService.saveUser(id, name, email, position)
+  const result = await userService.saveUser(id, name, email, position)
   res.json(result)
 })
 
